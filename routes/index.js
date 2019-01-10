3ed35729c093705265a9730a8e72f1ad7e596292
tanzil_afn <tanzil.afn@hotmail.com>
2019-01-11 02:18:33 +0600

    general url request
    Generalized url request
    although only 2 or 3 are working
    need to see why.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const https = require('https');
+const baseURL = "https://fantasy.premierleague.com/drf/";
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    // res.render('index', {title: 'Express'});
+const ProcessRequest = (urlExt ,cb) => {
+    console.log(baseURL+urlExt);
 
-    https.get('https://fantasy.premierleague.com/drf/bootstrap-static', (resp) => {
+    https.get(baseURL + urlExt, (resp) => {
         let data = '';
 
         // A chunk of data has been recieved.
@@ -14,15 +14,39 @@ router.get('/', function (req, res, next) {
             data += chunk;
         });
 
-        // The whole response has been received. Print out the result.
+        // The whole response has been received.
         resp.on('end', () => {
             // console.log(JSON.parse(data));
-            res.send(data);
+            // res.send(data);
+            cb(null, data);
         });
 
     }).on("error", (err) => {
         console.log("Error: " + err.message);
+        cb(err);
+    });
+};
+
+/* GET home page. */
+router.get('/*', function (req, res, next) {
+
+
+    console.log(req.url);
+    let url = req.url;
+    url = url.substring(1);
+    console.log("URL : ", url);
+
+    let rest = url.split("/");
+    console.log(rest);
+    ProcessRequest(url, (err, data)=>{
+        if(err){
+            console.log(err);
+            res.send(err);
+            return;
+        }
+        res.send(data);
     });
 });
+
 
 module.exports = router;
